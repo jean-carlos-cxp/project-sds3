a000dce7fcc77c4c87412c13619bb95714ed47a3
Jean Carlos <carlosjean090@gmail.com>
2021-05-09 01:05:21 -0300

    Pagination

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
         <>
             <NavBar />
             <div className="container">
-                <h1 className="text-primary py-3">Olá mundo!</h1>
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
                 <div className="row px-3">
                     <div className="row px-3">
